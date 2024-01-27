e51e9378dcd8aa730129f02567279f17eec274f6
Aleksei Fedorenko <fedorenksei@gmail.com>
2024-01-27 16:24:43 +0400

    feat(fs): implement create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
+import { writeFile } from "node:fs/promises";
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const create = async () => {
-    // Write your code here 
+  const freshFilePath = join(__dirname, "files/fresh.txt");
+
+  try {
+    await writeFile(freshFilePath, "I am fresh and young", { flag: "wx" });
+  } catch (err) {
+    if (err.code === "EEXIST") {
+      throw new Error("FS operation failed");
+    }
+    throw err;
+  }
 };
 
 await create();
