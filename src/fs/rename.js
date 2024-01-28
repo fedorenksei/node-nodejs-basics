948f201ff146865fd7f0969bb8adc364a1b8eb95
Aleksei Fedorenko <fedorenksei@gmail.com>
2024-01-28 15:36:35 +0400

    feat(fs): implement rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,31 @@
+import { access, rename as fsRename } from "node:fs/promises";
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const rename = async () => {
-    // Write your code here 
+  const oldNamePath = join(__dirname, "files/wrongFilename.txt");
+  const newNamePath = join(__dirname, "files/properFilename.md");
+
+  let doesNewPathExist;
+  try {
+    await access(newNamePath);
+    doesNewPathExist = true;
+  } catch {
+    // file on the new path shouldn't exist, thus access() has to throw an error
+  }
+  if (doesNewPathExist) {
+    throw new Error("FS operation failed");
+  }
+
+  try {
+    await fsRename(oldNamePath, newNamePath);
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      throw new Error("FS operation failed");
+    }
+    throw err;
+  }
 };
 
 await rename();
