9cd8800b1fc5090054882eaf4699350d7011dbc7
Aleksei Fedorenko <fedorenksei@gmail.com>
2024-01-28 15:45:47 +0400

    feat(fs): implement list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,19 @@
+import { readdir } from "node:fs/promises";
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const list = async () => {
-    // Write your code here 
+  const folderPath = join(__dirname, "files");
+
+  try {
+    console.log(await readdir(folderPath));
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      throw new Error("FS operation failed");
+    }
+    throw err;
+  }
 };
 
 await list();
