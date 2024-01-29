64636196d501a384e4b8c39a81572bc79a5b2363
Aleksei Fedorenko <fedorenksei@gmail.com>
2024-01-29 21:29:25 +0400

    fix(streams): add newlines to stdout in read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,12 +1,17 @@
 import { createReadStream } from "node:fs";
 import { dirname, join } from "node:path";
+import { stdout } from "node:process";
 import { fileURLToPath } from "node:url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const read = async () => {
   const fileToReadPath = join(__dirname, "files/fileToRead.txt");
 
-  createReadStream(fileToReadPath, "utf8").pipe(process.stdout);
+  const readable = createReadStream(fileToReadPath, "utf8");
+  readable.pipe(stdout);
+  readable.on("end", () => {
+    stdout.write("\n\n");
+  });
 };
 
 await read();
