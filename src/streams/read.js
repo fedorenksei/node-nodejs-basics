e8c616fb4c7c6516690abb41d547b26384bbe280
Aleksei Fedorenko <fedorenksei@gmail.com>
2024-01-29 21:21:59 +0400

    feat(streams): implement read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,12 @@
+import { createReadStream } from "node:fs";
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const read = async () => {
-    // Write your code here 
+  const fileToReadPath = join(__dirname, "files/fileToRead.txt");
+
+  createReadStream(fileToReadPath, "utf8").pipe(process.stdout);
 };
 
 await read();
